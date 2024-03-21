e37429bb1d9d05003209e6a45fbe04bf0b3274d7
pipistrellla <xexexexaxaxa22848@gmail.com>
2024-03-21 17:21:56 +0500

    добавил темную и светлую тему

--- advanced-app/src/App.tsx
+++ advanced-app/src/App.tsx
@@ -1,21 +1,30 @@
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useContext, useState } from 'react';
 // suspence позволяет показать пользователя, что идет загрузка
-import './index.scss';
+import './styles/index.scss';
 import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import {Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+
 interface AppProps {
     
 }
 // lazyload позволяет уменьшить размер бандла 
 // так как грузит не все страницы стразу
 
-const App: FC<AppProps> = ({  }) => {
-    return (
-        
-        <div className='app'>
 
+
+
+const App: FC<AppProps> = ({  }) => {
+
+    const {theme, toggleTheme} = useTheme();
+
+
+    return (
+        <div className={`app ${theme}`}>
+            <button onClick={()=> toggleTheme()}>Сменить тему</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О Нас</Link>
             {/* в fallback  указывается элемент который будет показыватся при загрузке */}
---
+++ advanced-app/src/theme/ThemeProvider.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useState, useMemo } from 'react'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+
+interface ThemeProviderProps {
+    
+}
+
+const defaultTheme:Theme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+
+const ThemeProvider: FC<ThemeProviderProps> = ({children }) => {
+        
+    const [theme, setTheme] = useState<Theme>(defaultTheme)
+
+
+
+    const defaultProps = useMemo(() => ({
+        theme: theme,
+        setTheme: setTheme 
+    }), [theme])
+
+    return (
+        <ThemeContext.Provider value = {defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeProvider;
